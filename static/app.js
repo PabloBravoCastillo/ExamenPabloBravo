c92f8839ddc818b28c44bd9d1ee0ec8af3958236
PabloBravoCastillo <91955829+PabloBravoCastillo@users.noreply.github.com>
2022-12-19 19:06:51 +0100

    P

--- static/app.js
+++ static/app.js
@@ -1,12 +1,14 @@
 // Pablo Bravo
-//
-// Completar JS aqui
-
 function cambiop1ap2(){
     document.getElementById('pestanaDietas').classList.add('oculto');
     document.getElementById('pestanaForm').classList.remove('oculto');
 }
 function cambiop2ap1(){
-    document.getElementById('pestanaForm').classList.remove('oculto');
-    document.getElementById('pestanaDietas').classList.add('oculto');
+    document.getElementById('pestanaForm').classList.add('oculto');
+    document.getElementById('pestanaDietas').classList.remove('oculto');
+}
+function enviarAlumno(){
+    document.getElementById('pestanaForm').classList.add('oculto');
+    document.getElementById('pestanaDietas').classList.remove('oculto');
+    alert("Datos guardados correctamente")
 }
