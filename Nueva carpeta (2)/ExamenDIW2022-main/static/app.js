62fcd8d592037863a739920f4cabed0eaf50bc02
PabloBravoCastillo <91955829+PabloBravoCastillo@users.noreply.github.com>
2022-12-19 11:54:39 +0100

    P

--- Nueva carpeta (2)/ExamenDIW2022-main/static/app.js
+++
@@ -1,3 +0,0 @@
-// nombre_del_alumno
-//
-// Completar JS aqui
---
+++ static/app.js
@@ -0,0 +1,12 @@
+// Pablo Bravo
+//
+// Completar JS aqui
+
+function cambiop1ap2(){
+    document.getElementById('pestanaDietas').classList.add('oculto');
+    document.getElementById('pestanaForm').classList.remove('oculto');
+}
+function cambiop2ap1(){
+    document.getElementById('pestanaForm').classList.remove('oculto');
+    document.getElementById('pestanaDietas').classList.add('oculto');
+}
